c7f34a060cef3ab94f668d30a5e2d566dd8e2b75
Victor Trejo <vic3jo@gmail.com>
2018-06-06 16:47:05 -0400

    Refactoring index

---
+++ controller.js
@@ -0,0 +1,107 @@
+const app = require('./dist/app.js');
+const fs = require('fs');
+const { buildQuery } = require('./app/helpers/query');
+
+const getDefinitions = (models) => {
+  return Object.keys(models)
+    .reduce(function (result, k) {
+      result[k] = models[k].attributes;
+      return result;
+    }, {});
+};
+
+const populate = async (resultPromise, modelName, sails) => {
+  const definition = getDefinitions(sails.models)[modelName];
+  const fieldsToPopulate = Object.keys(definition).filter(k => {
+      return definition[k].model || definition[k].collection;
+    }).map(k => k);
+  let result = resultPromise;
+  fieldsToPopulate.forEach(field => {
+    result = result.populate(field);
+  })
+  return result;
+};
+
+
+class Controller {
+  constructor(sails) {
+    this.sails = sails;
+    this.index = this.index.bind(this);
+    this.clientJS = this.clientJS.bind(this);
+    this.modelCount = this.modelCount.bind(this);
+    this.modelSearch = this.modelSearch.bind(this);
+    this.modelSearchAll = this.modelSearchAll.bind(this);
+    this.countAll = this.countAll.bind(this);
+  }
+
+  index(req, res) {
+    console.log(sails.config.appPath);
+    const injection = `
+      window.sailsModels = ${JSON.stringify(getDefinitions(this.sails.models))};
+      window.crudAdminConfig = ${JSON.stringify(this.sails.config.crudAdmin || {})};
+    `;
+    res.send(app.renderPage(injection));
+  }
+
+  clientJS(req, res) {
+    res.send(
+      fs.readFileSync(__dirname + '/dist/client.js')
+    );
+  }
+
+ async modelCount (req, res) {
+    const total = await this.sails.models[req.body.modelName].count({
+      where: buildQuery(req.body.queryRules || [])
+    });
+    return res.json(total);
+  }
+
+  async modelSearch(req, res) {
+    const resultPromise = this.sails.models[req.body.modelName].find({
+      where: buildQuery(req.body.queryRules || []),
+      skip: req.body.skip,
+      limit: req.body.limit,
+      sort: req.body.sort,
+    });
+    const result = await populate(
+      resultPromise,
+      req.body.modelName,
+      this.sails,
+    );
+    return res.json(result);
+  }
+
+  async modelSearchAll(req, res) {
+    const resultPromise = this.sails.models[req.query.modelName]
+      .find({});
+    const result = await populate(
+      resultPromise,
+      req.query.modelName,
+      this.sails
+    );
+    return res.json(result);
+  }
+
+  async countAll(req, res) {
+    const promises = Object.keys(this.sails.models).map(name => {
+      return new Promise((resolve) => {
+        this.sails.models[name].count({})
+          .then(count => {
+            resolve({ name, count });
+          });
+      });
+    });
+
+    const counts = await Promise.all(promises);
+    const result = counts.reduce((acc, item) => {
+      const x = acc;
+      x[item.name] = item.count;
+      return x;
+    }, {});
+    
+    return res.json(result);
+  }
+};
+
+
+module.exports = Controller;
--- index.js
+++ index.js
@@ -1,90 +1,17 @@
-const app = require('./dist/app.js');
-const fs = require('fs');
-const { buildQuery } = require('./app/helpers/query');
-
-const getDefinitions = (models) => {
-  return Object.keys(models)
-    .reduce(function (result, k) {
-      result[k] = models[k].attributes;
-      return result;
-    }, {});
-};
-
-const populate = async (resultPromise, modelName) => {
-  const definition = getDefinitions(sails.models)[modelName];
-  const fieldsToPopulate = Object.keys(definition).filter(k => {
-      return definition[k].model || definition[k].collection;
-    }).map(k => k);
-  let result = resultPromise;
-  fieldsToPopulate.forEach(field => {
-    result = result.populate(field);
-  })
-  return result;
-};
+const Controller = require('./controller');
 
 module.exports = function (sails) {
+  const controller = new Controller(sails);
+  
   return {
     routes: {
       before: {
-        'GET /administrator': function (req, res) {
-          const injection = `
-            window.sailsModels = ${JSON.stringify(getDefinitions(sails.models))};
-            window.crudAdminConfig = ${JSON.stringify(sails.config.crudAdmin || {})};
-          `;
-          res.send(app.renderPage(injection));
-        },
-        'GET /administrator/client.js': function (req, res) {
-          res.send(
-            fs.readFileSync(__dirname + '/dist/client.js')
-          );
-        },
-        'POST /administrator/model-count': async function (req, res) {
-          const total = await sails.models[req.body.modelName].count({
-            where: buildQuery(req.body.queryRules || [])
-          });
-          return res.json(total);
-        },
-        'POST /administrator/model-search': async function (req, res) {
-          const resultPromise = sails.models[req.body.modelName].find({
-            where: buildQuery(req.body.queryRules || []),
-            skip: req.body.skip,
-            limit: req.body.limit,
-            sort: req.body.sort,
-          });
-          const result = await populate(
-            resultPromise,
-            req.body.modelName
-          );
-          return res.json(result);
-        },
-        'GET /administrator/model-search-all': async function (req, res) {
-          const resultPromise = sails.models[req.query.modelName]
-            .find({});
-          const result = await populate(
-            resultPromise,
-            req.query.modelName
-          );
-          return res.json(result);
-        },
-        'GET /administrator/all-models-count': async function (req, res) {
-          const promises = Object.keys(sails.models).map(name => {
-            return new Promise((resolve) => {
-              sails.models[name].count({})
-                .then(count => {
-                  resolve({ name, count });
-                });
-            });
-          });
-
-          const counts = await Promise.all(promises);
-          const result = counts.reduce((acc, item) => {
-            const x = acc;
-            x[item.name] = item.count;
-            return x;
-          }, {});
-          
-          return res.json(result);
-        },
+        'GET /administrator': controller.index,
+        'GET /administrator/client.js': controller.clientJS,
+        'POST /administrator/model-count': controller.modelCount,
+        'POST /administrator/model-search': controller.modelSearch,
+        'GET /administrator/model-search-all': controller.modelSearchAll,
+        'GET /administrator/all-models-count': controller.countAll,
       },
     }
   };

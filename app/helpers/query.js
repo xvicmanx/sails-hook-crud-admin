68c12861074afafcf569c6d02d12c941b71293f8
Victor Trejo <vic3jo@gmail.com>
2018-06-05 20:32:58 -0400

    Fixing Boolean Query

--- app/helpers/query.js
+++ app/helpers/query.js
@@ -4,7 +4,6 @@ const mapCondition = (x) => {
     case 'ENDS_WITH': return 'endsWith';
     case 'BEGINS_WITH': return 'startsWith';
     case 'IS': return 'is';
-    case 'IS_NOT': return 'isNot';
     case 'EQUALS_TO': return 'equals';
     case 'IS_NOT_EQUALS_TO': return '!=';
     case 'DOES_NOT_BEGIN_WITH': return '!=';
@@ -23,18 +22,28 @@ const mapCondition = (x) => {
   }
 };
 
+const getValue = (rule) => {
+  if (
+    rule.type === 'date' &&
+    typeof rule.value === 'string'
+  ) {
+    return new Date(rule.value).getTime();
+  }
+  return rule.value;
+}
+
 const buildQuery = (rules) => {
   return rules.reduce((query, rule) => {
     const condition = mapCondition(rule.condition);
+    let value = getValue(rule);
+
     if (condition === 'equals') {
-      query[rule.field] = rule.value;
+      query[rule.field] = value;
     } else if (condition === 'is') {
-      query[rule.field] = true;
-    } else if (condition === 'isNot') {
-      query[rule.field] = false;
+      query[rule.field] = value;
     } else {
       query[rule.field] = {
-        [condition]: rule.value,
+        [condition]: value,
       };
     }
     
